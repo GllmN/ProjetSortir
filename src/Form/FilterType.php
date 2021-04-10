bc4ff6686b8de6ef6e5ff941895916398a3ec28d
GllmN <contact@guillaumeneau.com>
2021-04-10 23:14:46 +0200

    Guillaume : commit home.twig > filtre

--- src/Form/FilterType.php
+++ src/Form/FilterType.php
@@ -6,6 +6,7 @@ namespace App\Form;
 
 use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -20,13 +21,15 @@ class FilterType extends AbstractType
     {
         $builder
             ->add('campus', ChoiceType::class, [] )
-            ->add('keyWord', TextType::class, [] )
-            ->add('dateStart', DateType::class, [])
-            ->add('dateEnd', DateType::class, [])
-            ->add('eventOrganizer', ChoiceType::class, [])
-            ->add('eventSuscriber', ChoiceType::class, [])
-            ->add('eventNotSuscriber', ChoiceType::class, [])
-            ->add('eventOld', ChoiceType::class, [])
+            ->add('keyWord', TextType::class, [
+                'label'=>'Le nom de la sortie contient :',
+                'data' => 'Search'])
+            ->add('dateStart', DateType::class, ['label'=>'Entre :'])
+            ->add('dateEnd', DateType::class, ['label'=>'et :'])
+            ->add('eventOrganizer', CheckboxType::class, ['label'=>'Sortie dont je suis l organisateur/trice :'])
+            ->add('eventSuscriber', CheckboxType::class, ['label'=>'Sortie auquelles je suis inscrit/e :'])
+            ->add('eventNotSuscriber', CheckboxType::class, ['label'=>'Sortie auquelles je ne suis pas inscrit/e :'])
+            ->add('eventOld', CheckboxType::class, ['label'=>'Sortie passées :'])
             ->add('search', SubmitType::class, ['label'=>'Rechercher'])
         ;
     }
